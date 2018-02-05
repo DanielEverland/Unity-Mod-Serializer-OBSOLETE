aa64777c4086137745f1e924c3c0be75f8d7ce78
Daniel Everland <danieleverland@gmail.com>
2018-02-05 17:23:30 +0100

    Fixed issue where inherited types were not serialized

--- UMS/UnityModSerializer/Serialization/JsonSerializer.cs
+++ UMS/UnityModSerializer/Serialization/JsonSerializer.cs
@@ -12,7 +12,8 @@ namespace UMS.Serialization
 {
     public class JsonSerializer
     {
-        private static JsonSerializerSettings settings = new JsonSerializerSettings()
+        private static int errorIndex;
+        private static JsonSerializerSettings serializeSettings = new JsonSerializerSettings()
         {
             ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
             NullValueHandling = NullValueHandling.Ignore,
@@ -23,31 +24,68 @@ namespace UMS.Serialization
             ContractResolver = new OnlyFieldsContractResolver(),
         };
 
+        private static JsonSerializerSettings deserializeSettings = new JsonSerializerSettings()
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            TypeNameHandling = TypeNameHandling.All,
+            TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
+            TraceWriter = new Debugging.EG_TraceLogger(),
+        };
+
         public static string ToJson(object obj)
         {
-            return JsonConvert.SerializeObject(obj, Formatting.Indented, settings);
+            return JsonConvert.SerializeObject(obj, Formatting.Indented, serializeSettings);
         }
         public static object ToObject(string json)
         {
-            JObject jobject = JsonConvert.DeserializeObject<JObject>(json);
+            JObject jobject = ToObject<JObject>(json);
             Type type = jobject["$type"].ToObject<Type>();
 
             object obj = jobject.ToObject(type);
 
             if(obj is IModSerializer serializer)
             {
-                return CustomSerializers.DeserializeObject(obj);
+                try
+                {
+                    return CustomSerializers.DeserializeObject(obj);
+                }
+                catch (Exception)
+                {
+                    UnityEngine.Debug.LogError("Issue while deserializing object. Putting JSON on desktop");
+
+                    PasteJSONToDesktop(json);
+                    throw;
+                }                
             }
             
             return obj;
         }
         public static T ToObject<T>(string json)
         {
-            return JsonConvert.DeserializeObject<T>(json, settings);
+            return (T)ToObject(json, typeof(T));
         }
         public static object ToObject(string json, Type type)
         {
-            return JsonConvert.DeserializeObject(json, type, settings);
+            try
+            {
+                return JsonConvert.DeserializeObject(json, type, deserializeSettings);
+            }
+            catch (Exception)
+            {
+                UnityEngine.Debug.Log("Couldn't deserialize object. Putting JSON in desktop");
+
+                PasteJSONToDesktop(json);
+
+                throw;
+            }
+        }
+        private static void PasteJSONToDesktop(string json)
+        {
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = "/ERROR" + errorIndex++ + ".txt";
+            string fullPath = directory + fileName;
+
+            System.IO.File.WriteAllText(fullPath, json);
         }
         public static bool CanSerialize(Type type)
         {
@@ -68,8 +106,18 @@ namespace UMS.Serialization
     {
         protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
         {
-            return type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
-                .Select(x => base.CreateProperty(x, memberSerialization)).ToList();
+            List<JsonProperty> properties = new List<JsonProperty>();
+            Type toCheck = type;
+
+            while (toCheck != null && toCheck != typeof(object))
+            {
+                properties.AddRange(toCheck.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly)
+                .Select(x => base.CreateProperty(x, memberSerialization)));
+
+                toCheck = toCheck.BaseType;
+            }
+
+            return properties;
         }
     }
 }
