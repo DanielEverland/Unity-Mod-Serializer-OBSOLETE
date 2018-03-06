605821186f19722c2a29078c89311a22f6468b64
Daniel Everland <danieleverland@gmail.com>
2018-03-06 17:13:26 +0100

    Added a way to serialize any ScriptableObject with their data

---
+++ UMS/UnityModSerializer/Core/Types/SerializableScriptableObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace UMS.Core.Types
+{
+    public sealed class SerializableScriptableObject : SerializableObject<ScriptableObject, SerializableScriptableObject>
+    {
+        public override string Extension => "scriptableObject";
+
+        public SerializableScriptableObject() { }
+        public SerializableScriptableObject(ScriptableObject scriptableObject) : base(scriptableObject)
+        {
+            _type = scriptableObject.GetType();
+            _jsonString = JsonUtility.ToJson(scriptableObject, true);
+        }
+
+        [JsonProperty]
+        private Type _type;
+        [JsonProperty]
+        private string _jsonString;
+
+        public override ScriptableObject Deserialize(SerializableScriptableObject serializable)
+        {
+            ScriptableObject scriptableObject = ScriptableObject.CreateInstance(serializable._type);
+
+            JsonUtility.FromJsonOverwrite(serializable._jsonString, scriptableObject);
+
+            return scriptableObject;
+        }
+        public override SerializableScriptableObject Serialize(ScriptableObject obj)
+        {
+            return new SerializableScriptableObject(obj);
+        }
+    }
+}
