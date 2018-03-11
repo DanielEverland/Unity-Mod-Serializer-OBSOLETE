8022546c89f3d2b9e514f27825aafb4748218037
Daniel Everland <danieleverland@gmail.com>
2018-03-11 22:37:36 +0100

    Removed runtime declaration in Serializable

--- UMS/UnityModSerializer/Types/Serializable.cs
+++ UMS/UnityModSerializer/Types/Serializable.cs
@@ -10,7 +10,7 @@ namespace UMS.Types
     {
         public Type NonSerializableType => typeof(TFrom);
         public Type SerializedType => typeof(TTo);
-        public virtual int Priority => (int)Runtime.Core.Priority.Medium;
+        public virtual int Priority => (int)Core.Priority.Medium;
 
         public virtual TFrom Deserialize(TTo serializable) { throw new NotImplementedException(GetType().ToString()); }
         public virtual TTo Serialize(TFrom obj) { throw new NotImplementedException(GetType().ToString()); }
